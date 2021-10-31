25850dc7bb320d3d8135d906b7046c91570389d8
Notabot123 <37666014+Notabot123@users.noreply.github.com>
2021-10-31 15:02:12 +0000

    Add files via upload

---
+++ overclock/classes/parse_drawflow.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def parse_chart(data_json):
+    print('parsing')
+    print(data_json.connections)
--- overclock/main.py
+++ overclock/main.py
@@ -3,20 +3,35 @@ import numpy as np
 from random import randint
 from numpy.random import exponential, poisson
 from numpy.random import uniform as unf
-import json
+import pandas as pd
+import sys
+from classes.parse_drawflow import parse_chart
+
 
 eel.init("web")
 
 # Exposing the random_python function to javascript
-@eel.expose	# not used
+@eel.expose	# just example for sim result return
 def random_python():
 	print("Random function running")
 	return randint(1,100)
 
-@eel.expose	# not used
-def idealised_events():
-	print("idealised")
-	return randint(1,100)
+# Exposing the random_python function to javascript
+@eel.expose	
+def js_to_py(data_json):
+	""" Retrieve diagram from drawflow """
+	print("Diagram looks like this in JSON you see Python...")
+	# print(data_json)
+	parse_chart(data_json)
+	# return data_json
+
+@eel.expose	
+def read_table(data):
+	d = data.splitlines()
+	d = [i.split(',') for i in d]
+	df = pd.DataFrame(data=d)
+	h = df.to_html()
+	return h
 
 @eel.expose	
 def exprnd(mu=1):
@@ -33,7 +48,6 @@ def poissrnd(mu=1):
 @eel.expose	
 def unifrnd(mu=1):
 	e = unf(0,mu*2,500)
-	print(e)
 	eel.sleep(0.01)
 	return e.tolist()
 
@@ -43,8 +57,5 @@ def constrnd(mu=1):
 	eel.sleep(0.01)
 	return e.tolist()
 
-
-
 # Start the index.html file
-# eel.start("demo copy.html")
-eel.start("index copy.html")
+eel.start("index.html")
